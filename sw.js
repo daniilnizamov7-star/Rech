84d49707936aa0c7d3b010dca95e42f73bb626f1
daniilnizamov7-star <daniilnizamov7@gmail.com>
2026-05-27 21:38:20 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'dikcia-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.hostname === 'fonts.googleapis.com' || url.hostname === 'fonts.gstatic.com') {
+    e.respondWith(caches.open(CACHE).then(c => c.match(e.request).then(r => {
+      if (r) return r;
+      return fetch(e.request).then(resp => { c.put(e.request, resp.clone()); return resp; });
+    })));
+    return;
+  }
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      caches.open(CACHE).then(c => c.put(e.request, resp.clone()));
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
