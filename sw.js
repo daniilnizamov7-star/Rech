57c8aa9985446befc878a5b84021863b01a7cafa
daniilnizamov7-star <daniilnizamov7@gmail.com>
2026-05-28 06:50:25 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dikcia-v2';
+const CACHE = 'dikcia-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
